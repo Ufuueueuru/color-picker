d7a39f911281846c9836f6aaa8009377eb9fcdae
Ufuueueuru <theultimatewiseguy@gmail.com>
2018-01-29 17:30:24 -0700

    Update picker.js

--- picker.js
+++ picker.js
@@ -8,24 +8,24 @@ function setup() {
 function draw() {
 	//background(255-mouseX,255-mouseY,255-b);
 	background(150);
-	for(var r = 0;r < 255; r+=12) {
-		for(var g = 0;g < 255; g+=12) {
+	for(var r = 0;r < 255; r+=20) {
+		for(var g = 0;g < 255; g+=20) {
 			fill(r, g, b);
 			noStroke();
-			rect(r, g, 12, 12);
+			rect(r, g, 20, 20);
 		}
 	}
 	fill(mouseX, mouseY, b);
 	rect(260, 260, 35, 35);
-	for(var i = 0;i < 255;i += 12) {
+	for(var i = 0;i < 255;i += 10) {
 		fill(0, 0, i);
-		rect(260, i, 35, 12);
+		rect(260, i, 35, 10);
 	}
 	fill(0);
-	rect(260, b-12, 35, 12);
+	rect(260, b-10, 35, 5);
 	rect(constrain(mouseX,12,255)-3,constrain(mouseY,12,255)-3,6,6);
 	textSize(15);
-	text(constrain(mouseX,0,255) +"," + constrain(mouseY,0,255) + "," + constrain(b,0,255),10,280);
+	text(constrain(round(mouseX),0,255) +"," + constrain(mouseY,0,255) + "," + constrain(b,0,255),10,280);
 	if(mouseX >= 260 && mouseY <= 255 && mouseIsPressed && mouseX <= 295 && mouseY >= 0) {
 		b = mouseY;
 	}
